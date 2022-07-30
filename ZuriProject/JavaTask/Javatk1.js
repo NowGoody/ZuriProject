d9a4053b3dfb196d1235139a2da1794f2f37d973
NowGoody <96348811+NowGoody@users.noreply.github.com>
2022-07-30 20:40:43 +0100

    Add files via upload

---
+++ ZuriProject/JavaTask/Javatk1.js
@@ -0,0 +1,12 @@
+//My name
+let fullName = 'Gloria Oke';
+console.log(fullName);
+
+//Height value
+let height = 74;
+console.log(height);
+
+//Country name
+let countryName= 'Nigeria';
+console.log(countryName);
+
